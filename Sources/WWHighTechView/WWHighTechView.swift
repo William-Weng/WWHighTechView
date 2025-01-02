4c085ee28078929ffb83bfec8929221b390afc35
William.Weng <William.Weng@iit.com.hk>
2025-01-02 11:25:16 +0800

    - 修正動畫執行第二次後，怪怪的問題 - 加上背景色 / 閃動次數的設定值

--- Sources/WWHighTechView/WWHighTechView.swift
+++ Sources/WWHighTechView/WWHighTechView.swift
@@ -12,13 +12,16 @@ open class WWHighTechView: UIView {
     
     @IBOutlet var contentView: UIView!
     @IBOutlet var borderViews: [UIView]!
+    @IBOutlet var borderSubViews: [UIView]!
     @IBOutlet var distanceConstraints: [NSLayoutConstraint]!
-    
+
     @IBOutlet weak var containerView: UIView!
     
     private let transform3DRotation = (start: CATransform3DMakeRotation(CGFloat.pi * 0.48, 0, 1, 0), end: CATransform3DMakeRotation(CGFloat.pi * 0.0, 0, 1, 0))
     
     private var duration: CFTimeInterval = 0.5
+    private var repeatFlashCount: Double = 2.0
+    
     private var animationCount = 0
     private var animationKey: TransitionAnimationKeyPath?
 
@@ -51,11 +54,17 @@ public extension WWHighTechView {
     /// - Parameters:
     ///   - duration: [總執行時間 - 3組動畫](https://chillcomponent.codlin.me/components/card-futuristic/)
     ///   - innerView: 要加入的View
-    func start(duration: CFTimeInterval = 1.5, innerView: UIView) {
+    ///   - repeatFlashCount: 閃動動畫執行的次數
+    ///   - highTechColor: 主色調
+    func start(duration: CFTimeInterval = 1.5, innerView: UIView, repeatFlashCount: Double = 2.0, highTechColor: UIColor = UIColor(red: 126 / 255, green: 252 / 255, blue: 250, alpha: 1.0)) {
         
-        self.duration = duration / 3.0
+        self.animationKey = .opacity
+        self.repeatFlashCount = repeatFlashCount
+        self.duration = duration / (Double(borderViews.count) + repeatFlashCount - 1.0)
         
         innerView._autolayout(on: containerView)
+        
+        highTechColorSetting(highTechColor)
         borderViews.forEach { $0.isHidden = false }
         opacityAnimation(duration: self.duration)
     }
@@ -84,12 +93,9 @@ private extension WWHighTechView {
     
     /// 初始化一些View的位置設定
     func initViewSetting() {
-        
-        animationKey = .opacity
         containerView.isHidden = true
-
-        borderViews.forEach { $0.layer.transform = transform3DRotation.start; $0.isHidden = true }
-        distanceConstraints.forEach { $0.constant = contentView.bounds.height * 0.5 - (borderViews.first?.frame.height ?? 32.0) * 0.5 }
+        borderViews.forEach { $0.isHidden = true }
+        fixTransformAnimation()
     }
     
     /// 透明度動畫
@@ -97,9 +103,11 @@ private extension WWHighTechView {
     func opacityAnimation(duration: CFTimeInterval) {
         
         let keyPath: TransitionAnimationKeyPath = .opacity
-        let animation = CAAnimation._basicAnimation(keyPath: keyPath, delegate: self, fromValue: 1.0, toValue: 0.0, duration: duration, repeatCount: 2, isRemovedOnCompletion: true)
+        let animation = CAAnimation._basicAnimation(keyPath: keyPath, delegate: self, fromValue: 1.0, toValue: 0.0, duration: duration, repeatCount: Float(repeatFlashCount), isRemovedOnCompletion: true)
         
+        containerView.isHidden = true
         borderViews.forEach { $0.layer.add(animation, forKey: keyPath.rawValue) }
+        fixTransformAnimation()
     }
     
     /// y軸旋轉動畫
@@ -110,6 +118,9 @@ private extension WWHighTechView {
         let animation = CAAnimation._basicAnimation(keyPath: keyPath, delegate: self, fromValue: transform3DRotation.start, toValue: transform3DRotation.end, duration: duration)
         
         borderViews.forEach { $0.layer.add(animation, forKey: keyPath.rawValue) }
+        containerView.layer.add(animation, forKey: keyPath.rawValue)
+
+        distanceConstraints.forEach { $0.constant = contentView.bounds.height * 0.5 - (borderViews.first?.frame.height ?? 32.0) * 0.5 }
     }
     
     /// 外框更新上下間隔動畫
@@ -142,12 +153,35 @@ private extension WWHighTechView {
             if (animationCount != borderViews.count) { return }
             
             switch animationKey {
-            case .opacity: transformAnimation(duration: self.duration); animationKey = .transform
-            case .transform: constraintAnimation(duration: self.duration); animationKey = nil
+            case .opacity: transformAnimation(duration: duration); animationKey = .transform
+            case .transform: constraintAnimation(duration: duration); animationKey = nil
             default: animationKey = nil
             }
             
             animationCount = 0
         }
+    }
+    
+    /// 設定背景色
+    /// - Parameter backgroundColor: UIColor
+    func highTechColorSetting(_ backgroundColor: UIColor) {
+        containerView.backgroundColor = backgroundColor
+        borderSubViews.forEach { $0.backgroundColor = backgroundColor }
+    }
+    
+    /// 設定兩個外框最接近的距離
+    func distanceConstraintsSetting() {
+        distanceConstraints.forEach { $0.constant = contentView.bounds.height * 0.5 - (borderViews.first?.frame.height ?? 32.0) * 0.5 }
+    }
+    
+    /// 修正第二次之後，外框不能設定y軸旋轉的問題 (很玄，一定要用CAAnimation)
+    func fixTransformAnimation() {
+        
+        let keyPath: TransitionAnimationKeyPath = .transform
+        let animation = CAAnimation._basicAnimation(keyPath: keyPath, delegate: nil, fromValue: transform3DRotation.start, toValue: transform3DRotation.start, duration: duration)
+        
+        distanceConstraintsSetting()
+        borderViews.forEach { $0.layer.transform = transform3DRotation.start }
+        borderViews.forEach { $0.layer.add(animation, forKey: keyPath.rawValue) }
     }
 }
